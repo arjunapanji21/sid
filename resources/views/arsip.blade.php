617d4dd8cc31889bb692675d3a56c37defe83137
Arjuna Panji <arjuna.panji97@gmail.com>
2024-07-31 14:54:49 +0700

    init

--- resources/views/arsip.blade.php
+++ resources/views/arsip.blade.php
@@ -3,6 +3,45 @@
 @section('content')
 
 <h1 class="text-xl font-semibold">Arsip</h1>
-
+<section class="">
+    <div class="">
+        <!-- Start coding here -->
+        <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                <div class="w-full md:w-1/2">
+                    <form class="flex items-center">
+                        <label for="simple-search" class="sr-only">Search</label>
+                        <div class="relative w-full">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="overflow-x-auto min-h-80">
+                <table class="text-sm w-full text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-4 py-3">Tgl. Pengajuan</th>
+                            <th scope="col" class="px-4 py-3">Diajukan Oleh</th>
+                            <th scope="col" class="px-4 py-3">Jenis Surat</th>
+                            <th scope="col" class="px-4 py-3">Status</th>
+                            <th scope="col" class="px-4 py-3">
+                                <span class="sr-only">Actions</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    </section>
 
 @endsection
--- resources/views/buat_surat.blade.php
+++ resources/views/buat_surat.blade.php
@@ -2,7 +2,108 @@
 
 @section('content')
 
-<h1 class="text-xl font-semibold">Pembuatan Surat</h1>
-
-
+<section class="">
+    <div class="">
+        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Pembuatan Surat</h2>
+        <form action="{{route('upload_data_warga')}}" method="post">
+            @csrf
+            <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                <div class="w-full">
+                    <label for="jenis_surat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis
+                        Surat</label>
+                    <select id="jenis_surat" name="jenis_surat"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <option selected disabled>Pilih jenis surat:</option>
+                        <option selected value="Surat Keterangan Kematian">Surat Keterangan Kematian</option>
+                    </select>
+                </div>
+                <div class="w-full">
+                    <label for="nama_lengkap_alias" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Lengkap dan Alias</label>
+                    <input type="text" name="nama_lengkap_alias" id="nama_lengkap_alias"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Nama lengkap dan alias alm/h" required="">
+                </div>
+                <div class="w-full">
+                    <label for="jk" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis
+                        Kelamin</label>
+                    <select id="jk" name="jk"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <option selected disabled>Pilih jenis kelamin:</option>
+                        <option value="Laki-laki">Laki-laki</option>
+                        <option value="Perempuan">Perempuan</option>
+                    </select>
+                </div>
+                <div class="w-full">
+                    <label for="tempat_lahir"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tempat Lahir</label>
+                    <input type="text" name="tempat_lahir" id="tempat_lahir"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Tempat lahir alm/h" required="">
+                </div>
+                <div class="w-full">
+                    <label for="tgl_lahir" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal
+                        Lahir</label>
+                    <input type="date" name="tgl_lahir" id="tgl_lahir"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Tanggal lahir sesuai KTP" required="">
+                </div>
+                <div>
+                    <label for="kewarganegaraan"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kewarganegaraan</label>
+                    <select id="kewarganegaraan" name="kewarganegaraan"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <option selected disabled>Pilih kewarganegaraan:</option>
+                        <option value="WNI">WNI</option>
+                        <option value="WNA">WNA</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="agama"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Agama</label>
+                    <select id="agama" name="agama"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <option selected disabled>Pilih agama:</option>
+                        <option value="Islam">Islam</option>
+                        <option value="Kristen">Kristen</option>
+                        <option value="Katolik">Katolik</option>
+                        <option value="Hindu">Hindu</option>
+                        <option value="Buddha">Buddha</option>
+                        <option value="Khonghucu">Khonghucu</option>
+                    </select>
+                </div>
+                <div class="w-full">
+                    <label for="pekerjaan"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pekerjaan</label>
+                    <input type="text" name="pekerjaan" id="pekerjaan"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Pekerjaan alm/h" required="">
+                </div>
+                <div class="w-full">
+                    <label for="tgl_wafat"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tgl. Wafat</label>
+                    <input type="text" name="tgl_wafat" id="tgl_wafat"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Tanggal wafat alm/h" required="">
+                </div>
+                <div class="w-full">
+                    <label for="nama_orangtua"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Orang Tua</label>
+                    <input type="text" name="nama_orangtua" id="nama_orangtua"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Nama orang tua alm/h" required="">
+                </div>
+            </div>
+            <div class="flex gap-2 justify-end">
+                <button type="submit"
+                    class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                    Kirim
+                </button>
+                <a href="{{route('data_warga')}}"
+                    class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-primary-700  rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-gray-200">
+                    Batalkan
+                </a>
+            </div>
+        </form>
+    </div>
+</section>
 @endsection
